b56430aa290de9e5e4b7e9640dad54001feb8057
star-bits <star-bits@outlook.com>
2023-04-03 00:08:10 +0900

    port=5001

--- app.py
+++ app.py
@@ -47,5 +47,5 @@ def review_get():
     return jsonify({'get_api_return': all_reviews})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5000, debug=True)
+   app.run('0.0.0.0', port=5001, debug=True)
    # app.run()
