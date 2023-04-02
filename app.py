9c57181a9419e0da91b40498eb18e544d5308304
star-bits <star-bits@outlook.com>
2023-04-03 00:05:26 +0900

    final

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://sparta:test@cluster0.csrgvbd.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
+import requests
+from bs4 import BeautifulSoup
+
 @app.route('/')
 def home():
    return render_template('index.html')
@@ -17,10 +20,20 @@ def review_post():
     star_receive = request.form['star_give']
     review_receive = request.form['review_give']
 
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url_receive,headers=headers)
+
+    soup = BeautifulSoup(data.text, 'html.parser')
+
+    ogtitle = soup.select_one('meta[property="og:title"]')['content']
+    ogimage = soup.select_one('meta[property="og:image"]')['content']
+
     doc = {
         'url': url_receive,
         'star': star_receive,
-        'review': review_receive
+        'review': review_receive,
+        'title': ogtitle,
+        'image': ogimage
     }
     db.review_db.insert_one(doc)
 
@@ -34,5 +47,5 @@ def review_get():
     return jsonify({'get_api_return': all_reviews})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', port=5001, debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
    # app.run()
