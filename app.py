01d3973924bcddd94461797b77c63f5290b3604d
star-bits <93939472+star-bits@users.noreply.github.com>
2023-04-03 09:07:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,9 +9,12 @@ db = client.dbsparta
 import requests
 from bs4 import BeautifulSoup
 
+
+
 @app.route('/')
 def home():
    return render_template('index.html')
+
 
 # 2. POST (저장하기)
 @app.route("/review_api", methods=["POST"])
@@ -22,7 +25,6 @@ def review_post():
 
     headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
     data = requests.get(url_receive,headers=headers)
-
     soup = BeautifulSoup(data.text, 'html.parser')
 
     ogtitle = soup.select_one('meta[property="og:title"]')['content']
@@ -37,15 +39,17 @@ def review_post():
     }
     db.review_db.insert_one(doc)
 
-    return jsonify({'post_api_return': '저장하기 완료!(POST 연결 완료!)'})
+    return jsonify({'post_api_return': '저장하기 완료! (POST 연결 완료!)'})
 
+   
 # 3. GET (가져오기)
 @app.route("/review_api", methods=["GET"])
 def review_get():
     all_reviews = list(db.review_db.find({},{'_id':False}))
-    print(all_reviews)
+    # print(all_reviews)
     return jsonify({'get_api_return': all_reviews})
 
+   
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5001, debug=True)
    # app.run()
